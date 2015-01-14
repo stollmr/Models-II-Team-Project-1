b7fb394bcd309b70dc3923eeb4cc82def26995ed
aldermde <aldermde@mail.uc.edu>
2015-01-14 15:32:22 -0500

    Update Team_Project_I.m
    Added initial menu statements.

--- Team_Project_I.m
+++ Team_Project_I.m
@@ -1,2 +1,17 @@
 %Models II Team Project I
-%Group: 
+%Group:
+
+clear;
+clc;
+load Electricity_Data.mat;
+
+analizeChoice = menu('How would you like to analze the electricity data?','By Year', 'By Country', 'By Production Type');
+
+switch analizeChoice
+    case 1
+        yearChoice = menu('Please select a year:',' )
+    case 2
+        countryChoice = menu('Please select a country:', ' )
+    case 3
+        productionChoice = menu('Please select a production type:', ' )
+end
