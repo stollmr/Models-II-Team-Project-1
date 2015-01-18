3faefbf2937b0c110c34a6695a9f39d262b8d5ec
aldermde <aldermde@mail.uc.edu>
2015-01-18 18:50:07 -0500

    Update Team_Project_I.m
    Case 2 section done.

--- Team_Project_I.m
+++ Team_Project_I.m
@@ -40,7 +40,64 @@ switch analyzeChoice
             elseif countryChoice == 11
                 countryChoice = 'USSR';
             end
-        %Analyze data here
+                    %Total energy production / demand
+        for k = 1990:2011
+            production(k-1989) = Electricity_Data.Total_Production(find(Electricity_Data.Year == k & strcmp(Electricity_Data.Country, countryChoice) == 1));
+            demand(k-1989) = Electricity_Data.Demand(find(Electricity_Data.Year == k & strcmp(Electricity_Data.Country, countryChoice) == 1));
+            year(k-1989) = k;
+        end
+        
+        figure;
+        subplot(2,2,1);
+        plot(year, production, '-b*', year, demand, '-ro');
+        title('Total Energy Production and Demand by Year');
+        xlabel('Year');ylabel('Energy (Million kWh)');
+        legend('Energy Production', 'Energy Demand','Location','northwest');
+        %Line graph of production types per year
+        for l = 1990:2011
+            productionHydro(l-1989) = Electricity_Data.Hydro_Production(find(Electricity_Data.Year == l & strcmp(Electricity_Data.Country, countryChoice) == 1));
+            if productionHydro(l-1989) == -9999
+                productionHydro(l-1989) = 0;
+            end
+            productionSolar(l-1989) = Electricity_Data.Solar_Production(find(Electricity_Data.Year == l & strcmp(Electricity_Data.Country, countryChoice) == 1));
+            if productionSolar(l-1989) == -9999
+                productionSolar(l-1989) = 0;
+            end
+            productionWind(l-1989) = Electricity_Data.Wind_Production(find(Electricity_Data.Year == l & strcmp(Electricity_Data.Country, countryChoice) == 1));
+            if productionWind(l-1989) == -9999
+                productionWind(l-1989) = 0;
+            end
+            productionNuclear(l-1989) = Electricity_Data.Nuclear_Production(find(Electricity_Data.Year == l & strcmp(Electricity_Data.Country, countryChoice) == 1));
+            if productionNuclear(l-1989) == -9999
+                productionNuclear(l-1989) = 0;
+            end
+        end
+        
+        subplot(2,2,2);
+        plot(year,productionHydro,'-b+', year,productionSolar,'-ro', year,productionWind,'-g.', year, productionNuclear,'-m^');
+        title('Energy Production Per Year by Type');
+        xlabel('Year');ylabel('Energy (Million kWh)');
+        legend('Hydro','Solar','Wind','Nuclear');
+        %Pie chart of production types
+        avgHydro = mean(productionHydro);
+        avgSolar = mean(productionSolar);
+        avgWind = mean(productionWind);
+        avgNuclear = mean(productionNuclear);
+        
+        subplot(2,2,3)
+        avgProduction = [avgHydro avgSolar avgWind avgNuclear];
+        pie(avgProduction);
+        title('Average Energy Production Distribution');
+        legend('Hydro','Solar','Wind','Nuclear');
+        %Energy surplus and deficit
+        for p = 1:22
+            surplus(p) = production(p) - demand(p);
+        end
+        
+        subplot(2,2,4);
+        bar(year, surplus);
+        title('Energy Surplus Per Year');
+        xlabel('Year');ylabel('Energy (Million kWh)');
         end
     case 3
         productionChoice = menu('Please select a production type:', 'Hyrdo','Solar','Wind','Nuclear','Total Production' )
